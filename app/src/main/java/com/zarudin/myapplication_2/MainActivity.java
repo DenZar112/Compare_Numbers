81866156ed0af6b887eb74b6275bbd667a9807a4
Denis <deza56@gmail.com>
2021-05-04 21:43:49 +0400

    Add button

--- app/src/main/java/com/zarudin/myapplication_2/MainActivity.java
+++ app/src/main/java/com/zarudin/myapplication_2/MainActivity.java
@@ -3,6 +3,9 @@ package com.zarudin.myapplication_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button_2);
+        button.setOnClickListener(v -> Toast.makeText(MainActivity.this, "PUSH",
+                Toast.LENGTH_SHORT).show());
     }
 }
