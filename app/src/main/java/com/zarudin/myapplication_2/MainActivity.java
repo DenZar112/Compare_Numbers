1a8b152f64dc6ebe361ddad1fd88425a3dafaba6
Denis <deza56@gmail.com>
2021-05-05 10:53:04 +0400

    Stable compare

--- app/src/main/java/com/zarudin/myapplication_2/MainActivity.java
+++ app/src/main/java/com/zarudin/myapplication_2/MainActivity.java
@@ -17,19 +17,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
         setContentView(R.layout.compare);
 
-        Button button = findViewById(R.id.button_2);
-        button.setOnClickListener(v -> Toast.makeText(MainActivity.this, "PUSH",
-                Toast.LENGTH_SHORT).show());
 
         Button button3 = findViewById(R.id.button3);
         final EditText editText1 = findViewById(R.id.editText1);
         final EditText editText2 = findViewById(R.id.editText2);
         final TextView textView = findViewById(R.id.textView);
 
-        button.setOnClickListener(v -> {
+        button3.setOnClickListener(v -> {
             arg1 = Integer.valueOf(editText1.getText().toString());
             arg2 = Integer.valueOf(editText2.getText().toString());
             if (arg1.equals(arg2)) {
