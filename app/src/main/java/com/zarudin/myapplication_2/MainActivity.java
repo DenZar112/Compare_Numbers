36a2ed5d6d60bc98f6f5a40274147c1625c4f3fe
Denis <deza56@gmail.com>
2021-05-05 09:22:07 +0400

    Add Compare

--- app/src/main/java/com/zarudin/myapplication_2/MainActivity.java
+++ app/src/main/java/com/zarudin/myapplication_2/MainActivity.java
@@ -5,17 +5,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Integer arg1;
+    private Integer arg2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.compare);
 
         Button button = findViewById(R.id.button_2);
         button.setOnClickListener(v -> Toast.makeText(MainActivity.this, "PUSH",
                 Toast.LENGTH_SHORT).show());
+
+        Button button3 = findViewById(R.id.button3);
+        final EditText editText1 = findViewById(R.id.editText1);
+        final EditText editText2 = findViewById(R.id.editText2);
+        final TextView textView = findViewById(R.id.textView);
+
+        button.setOnClickListener(v -> {
+            arg1 = Integer.valueOf(editText1.getText().toString());
+            arg2 = Integer.valueOf(editText2.getText().toString());
+            if (arg1.equals(arg2)) {
+                textView.setText("Равно!");
+            } else {
+                textView.setText("Не равно!");
+            }
+
+        });
     }
+
+
 }
+
+
+
